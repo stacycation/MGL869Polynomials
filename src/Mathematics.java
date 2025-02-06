1eb26795a10ebdc5f7c91c97af36917eea078a2a
stacycation <c.stacy06@gmail.com>
2025-02-06 01:33:04 -0500

    exercises 1-13 complete

--- src/Mathematics.java
+++ src/Mathematics.java
@@ -1,7 +1,13 @@
 
 public class Mathematics {
 	
+	private Polynomial polynomial;
+	private Term[] polynomialArray;
+		
 	public Polynomial simplify(Polynomial polynomial) {
+		this.polynomial = polynomial;
+		polynomialArray = polynomial.getAllTerms();
+		
 		
 	}
 }
